a79d3f0373aca3523f9359d8045ec34781976737
Evan Feenstra <evan@evanfeenstra.com>
2020-08-11 11:30:42 -0700

    upload files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player'
+import {useDropzone} from 'react-dropzone'
 
 const songs = [
   {name:'Lemon', file:'LEMON.wav'},
@@ -19,12 +20,37 @@ function App(){
         }}
       />
     })}
+    <div className="upload">
+      <MyDropzone />
+    </div>
   </div>
+}
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    console.log(acceptedFiles)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  return (
+    <div {...getRootProps()} style={{
+        fontSize:11, padding:30, width:300, textAlign:'center',
+        color:isDragActive?'white':'grey',
+        border:isDragActive?'2px dashed white':'2px dashed grey'
+      }}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 }
 
 function Song(props){
   const {song, selected, onSelect} = props
-  return <div className="song">
+  return <div className="song" style={{background:selected?'navy':'black'}}>
     <div onClick={onSelect} className="song-name">
       {song.name}
     </div>
